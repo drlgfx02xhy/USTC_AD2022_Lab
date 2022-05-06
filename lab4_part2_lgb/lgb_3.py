a483533ca4b1a64f72f8c40db344470e867c3de8
Daniel Xu <xuhaiyang@mail.ustc.edu.cn>
2022-05-06 23:58:53 +0800

    see cross-k evaluation performance: not bad

--- lab4_part2_lgb/lgb_3.py
+++ lab4_part2_lgb/lgb_3.py
@@ -34,3 +34,18 @@ def get_config(config_file):
 	SEARCH_params.merge_from_file(os.path.join('./config_3', config_file))
 	params = {**FIXED_params, **SEARCH_params}
 	return params
+
+def get_single_data():
+	# 划分数据集
+	data = np.load('./data/DatawithrawL.npy')
+	test = data[:int(data.shape[0]*0.2), :]
+	train = data[int(data.shape[0]*0.2):, :]
+	train_x = train[:, :-1]
+	train_label = train[:, -1]
+	test_x = test[:, :-1]
+	test_label = test[:, -1]
+
+	# 转换为Dataset数据格式
+	lgb_train = lgb.Dataset(train_x, train_label)
+	lgb_eval = lgb.Dataset(test_x, test_label, reference=lgb_train)
+	return test_x, test_label, lgb_train, lgb_eval
